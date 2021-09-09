ba948014a15282e6c5b52e108982920c23ac60de
sushant2106 <90310743+sushant2106@users.noreply.github.com>
2021-09-09 23:09:42 +0530

    day3 work done

---
+++ day3/second.js
@@ -0,0 +1,14 @@
+
+function addTask()
+{
+   let data= document.getElementById('inp').value;
+   document.getElementById('two').innerHTML += `<h1 class='todo'>${data}</h1>`;
+   document.getElementById('inp').value=" ";
+   
+}
+function change()
+{
+    document.getElementById('two').style.backgroundColor=" yellow";
+}
+
+
